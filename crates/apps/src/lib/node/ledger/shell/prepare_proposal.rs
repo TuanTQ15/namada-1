hkey0/namada

First compiling version

crates/apps/src/lib/node/ledger/shell/prepare_proposal.rs
@@ -1,13 +1,11 @@
 //! Implementation of the [`RequestPrepareProposal`] ABCI++ method for the Shell
 
 use masp_primitives::transaction::Transaction;
-use namada::core::hints;
 use namada::gas::TxGasMeter;
 use namada::ledger::protocol;
-use namada::ledger::storage::tx_queue::TxInQueue;
 use namada::proof_of_stake::storage::find_validator_by_raw_hash;
 use namada::state::{DBIter, StorageHasher, TempWlStorage, DB};
-use namada::tx::data::{DecryptedTx, TxType, WrapperTx};
+use namada::tx::data::{TxType, WrapperTx};
 use namada::tx::Tx;
 use namada::types::address::Address;
 use namada::types::key::tm_raw_hash_to_string;
@@ -17,8 +15,8 @@ use namada::vm::WasmCacheAccess;
 
 use super::super::*;
 use super::block_alloc::states::{
-    BuildingDecryptedTxBatch, BuildingProtocolTxBatch,
-    EncryptedTxBatchAllocator, NextState, TryAlloc,
+    BuildingProtocolTxBatch, BuildingTxBatch, NextState, TryAlloc,
+    WithNormalTxs, WithoutNormalTxs,
 };
 use super::block_alloc::{AllocFailure, BlockAllocator, BlockResources};
 use crate::config::ValidatorLocalConfig;
@@ -39,17 +37,21 @@ where
     ///
     /// INVARIANT: Any changes applied in this method must be reverted if
     /// the proposal is rejected (unless we can simply overwrite
-    /// them in the next block).
+    /// them in the next block). Furthermore, protocol transactions cannot
+    /// affect the ability of a tx to pay its wrapper fees.
     pub fn prepare_proposal(
         &self,
-        req: RequestPrepareProposal,
+        mut req: RequestPrepareProposal,
     ) -> response::PrepareProposal {
         let txs = if let ShellMode::Validator {
             ref local_config, ..
         } = self.mode
         {
             // start counting allotted space for txs
-            let alloc = self.get_encrypted_txs_allocator();
+            let alloc = self.get_protocol_txs_allocator();
+            // add vote extension protocol txs
+            let (alloc, mut txs) = self.build_protocol_txs(alloc, &mut req.txs);
+            let alloc = alloc.next_state();
 
             // add encrypted txs
             let tm_raw_hash_string =
@@ -63,22 +65,18 @@ where
                 "Unable to find native validator address of block proposer \
                  from tendermint raw hash",
             );
-            let (encrypted_txs, alloc) = self.build_encrypted_txs(
+            let (mut normal_txs, alloc) = self.build_normal_txs(
                 alloc,
                 &req.txs,
                 req.time,
                 &block_proposer,
                 local_config.as_ref(),
             );
-            let mut txs = encrypted_txs;
+            txs.append(&mut normal_txs);
             // decrypt the wrapper txs included in the previous block
-            let (mut decrypted_txs, alloc) = self.build_decrypted_txs(alloc);
-            txs.append(&mut decrypted_txs);
-
-            // add vote extension protocol txs
-            let mut protocol_txs = self.build_protocol_txs(alloc, &req.txs);
-            txs.append(&mut protocol_txs);
-
+            let (_, mut remaining_txs) =
+                self.build_protocol_txs(alloc, &mut req.txs);
+            txs.append(&mut remaining_txs);
             txs
         } else {
             vec![]
@@ -93,49 +91,28 @@ where
         response::PrepareProposal { txs }
     }
 
-    /// Depending on the current block height offset within the epoch,
-    /// transition state accordingly, return a block space allocator
-    /// with or without encrypted txs.
-    ///
-    /// # How to determine which path to take in the states DAG
-    ///
-    /// If we are at the second or third block height offset within an
-    /// epoch, we do not allow encrypted transactions to be included in
-    /// a block, therefore we return an allocator wrapped in an
-    /// [`EncryptedTxBatchAllocator::WithoutEncryptedTxs`] value.
-    /// Otherwise, we return an allocator wrapped in an
-    /// [`EncryptedTxBatchAllocator::WithEncryptedTxs`] value.
+    /// Get the first state of the block allocator. This is for protocol
+    /// transactions.
     #[inline]
-    fn get_encrypted_txs_allocator(&self) -> EncryptedTxBatchAllocator {
-        let is_2nd_height_off = self.is_deciding_offset_within_epoch(1);
-        let is_3rd_height_off = self.is_deciding_offset_within_epoch(2);
-
-        if hints::unlikely(is_2nd_height_off || is_3rd_height_off) {
-            tracing::warn!(
-                proposal_height =
-                    ?self.wl_storage.storage.block.height,
-                "No mempool txs are being included in the current proposal"
-            );
-            EncryptedTxBatchAllocator::WithoutEncryptedTxs(
-                (&self.wl_storage).into(),
-            )
-        } else {
-            EncryptedTxBatchAllocator::WithEncryptedTxs(
-                (&self.wl_storage).into(),
-            )
-        }
+    fn get_protocol_txs_allocator(
+        &self,
+    ) -> BlockAllocator<BuildingProtocolTxBatch<WithNormalTxs>> {
+        (&self.wl_storage).into()
     }
 
     /// Builds a batch of encrypted transactions, retrieved from
     /// Tendermint's mempool.
-    fn build_encrypted_txs(
+    fn build_normal_txs(
         &self,
-        mut alloc: EncryptedTxBatchAllocator,
+        mut alloc: BlockAllocator<BuildingTxBatch>,
         txs: &[TxBytes],
         block_time: Option<Timestamp>,
         block_proposer: &Address,
         proposer_local_config: Option<&ValidatorLocalConfig>,
-    ) -> (Vec<TxBytes>, BlockAllocator<BuildingDecryptedTxBatch>) {
+    ) -> (
+        Vec<TxBytes>,
+        BlockAllocator<BuildingProtocolTxBatch<WithoutNormalTxs>>,
+    ) {
         let block_time = block_time.and_then(|block_time| {
             // If error in conversion, default to last block datetime, it's
             // valid because of mempool check
@@ -196,85 +173,24 @@ where
         (txs, alloc)
     }
 
-    /// Builds a batch of DKG decrypted transactions.
-    // NOTE: we won't have frontrunning protection until V2 of the
-    // Anoma protocol; Namada runs V1, therefore this method is
-    // essentially a NOOP
-    //
-    // sources:
-    // - https://specs.namada.net/main/releases/v2.html
-    // - https://github.com/anoma/ferveo
-    fn build_decrypted_txs(
-        &self,
-        mut alloc: BlockAllocator<BuildingDecryptedTxBatch>,
-    ) -> (Vec<TxBytes>, BlockAllocator<BuildingProtocolTxBatch>) {
-        let txs = self
-            .wl_storage
-            .storage
-            .tx_queue
-            .iter()
-            .map(
-                |TxInQueue {
-                     tx,
-                     gas: _,
-                }| {
-                    let mut tx = tx.clone();
-                    tx.update_header(TxType::Decrypted(DecryptedTx::Decrypted));
-                    tx.to_bytes().into()
-                },
-            )
-            // TODO: make sure all decrypted txs are accepted
-            .take_while(|tx_bytes: &TxBytes| {
-                alloc.try_alloc(&tx_bytes[..]).map_or_else(
-                    |status| match status {
-                        AllocFailure::Rejected { bin_resource_left: bin_space_left } => {
-                            tracing::warn!(
-                                ?tx_bytes,
-                                bin_space_left,
-                                proposal_height =
-                                    ?self.get_current_decision_height(),
-                                "Dropping decrypted tx from the current proposal",
-                            );
-                            false
-                        }
-                        AllocFailure::OverflowsBin { bin_resource: bin_size } => {
-                            tracing::warn!(
-                                ?tx_bytes,
-                                bin_size,
-                                proposal_height =
-                                    ?self.get_current_decision_height(),
-                                "Dropping large decrypted tx from the current proposal",
-                            );
-                            true
-                        }
-                    },
-                    |()| true,
-                )
-            })
-            .collect();
-        let alloc = alloc.next_state();
-
-        (txs, alloc)
-    }
-
     /// Builds a batch of protocol transactions.
-    fn build_protocol_txs(
+    fn build_protocol_txs<M>(
         &self,
-        mut alloc: BlockAllocator<BuildingProtocolTxBatch>,
-        txs: &[TxBytes],
-    ) -> Vec<TxBytes> {
+        mut alloc: BlockAllocator<BuildingProtocolTxBatch<M>>,
+        txs: &mut Vec<TxBytes>,
+    ) -> (BlockAllocator<BuildingProtocolTxBatch<M>>, Vec<TxBytes>) {
         if self.wl_storage.storage.last_block.is_none() {
             // genesis should not contain vote extensions.
             //
             // this is because we have not decided any block through
             // consensus yet (hence height 0), which in turn means we
             // have not committed any vote extensions to a block either.
-            return vec![];
+            return (alloc, vec![]);
         }
 
-        let deserialized_iter = self.deserialize_vote_extensions(txs);
+        let mut deserialized_iter = self.deserialize_vote_extensions(txs);
 
-        deserialized_iter.take_while(|tx_bytes|
+        let taken = deserialized_iter.by_ref().take_while(|tx_bytes|
             alloc.try_alloc(&tx_bytes[..])
                 .map_or_else(
                     |status| match status {
@@ -312,7 +228,9 @@ where
                     |()| true,
                 )
         )
-        .collect()
+        .collect();
+        deserialized_iter.keep_rest();
+        (alloc, taken)
     }
 }
 
@@ -430,8 +348,6 @@ where
 mod test_prepare_proposal {
     use std::collections::BTreeSet;
 
-    use borsh_ext::BorshSerializeExt;
-    use namada::ledger::gas::Gas;
     use namada::ledger::pos::PosQueries;
     use namada::ledger::replay_protection;
     use namada::proof_of_stake::storage::{
@@ -444,8 +360,8 @@ mod test_prepare_proposal {
     use namada::token;
     use namada::token::{read_denom, Amount, DenominatedAmount};
     use namada::tx::data::{Fee, TxType, WrapperTx};
-    use namada::tx::{Code, Data, Header, Section, Signature, Signed};
-    use namada::types::address::{self, Address};
+    use namada::tx::{Code, Data, Section, Signature, Signed};
+    use namada::types::address;
     use namada::types::ethereum_events::EthereumEvent;
     use namada::types::key::RefTo;
     use namada::types::storage::{BlockHeight, InnerEthEventsQueue};
@@ -483,7 +399,7 @@ mod test_prepare_proposal {
     #[test]
     fn test_prepare_proposal_rejects_non_wrapper_tx() {
         let (shell, _recv, _, _) = test_utils::setup();
-        let mut tx = Tx::from_type(TxType::Decrypted(DecryptedTx::Decrypted));
+        let mut tx = Tx::from_type(TxType::Raw);
         tx.header.chain_id = shell.chain_id.clone();
         let req = RequestPrepareProposal {
             txs: vec![tx.to_bytes().into()],
@@ -772,98 +688,6 @@ mod test_prepare_proposal {
         };
 
         assert_eq!(signed_eth_ev_vote_extension, rsp_ext.0);
-    }
-
-    /// Test that the decrypted txs are included
-    /// in the proposal in the same order as their
-    /// corresponding wrappers
-    #[test]
-    fn test_decrypted_txs_in_correct_order() {
-        let (mut shell, _recv, _, _) = test_utils::setup();
-        let keypair = gen_keypair();
-        let mut expected_wrapper = vec![];
-        let mut expected_decrypted = vec![];
-
-        // Load some tokens to tx signer to pay fees
-        let balance_key = token::storage_key::balance_key(
-            &shell.wl_storage.storage.native_token,
-            &Address::from(&keypair.ref_to()),
-        );
-        shell
-            .wl_storage
-            .storage
-            .write(&balance_key, Amount::native_whole(1_000).serialize_to_vec())
-            .unwrap();
-
-        let mut req = RequestPrepareProposal {
-            txs: vec![],
-            ..Default::default()
-        };
-        // create a request with two new wrappers from mempool and
-        // two wrappers from the previous block to be decrypted
-        for i in 0..2 {
-            let mut tx =
-                Tx::from_type(TxType::Wrapper(Box::new(WrapperTx::new(
-                    Fee {
-                        amount_per_gas_unit: DenominatedAmount::native(
-                            1.into(),
-                        ),
-                        token: shell.wl_storage.storage.native_token.clone(),
-                    },
-                    keypair.ref_to(),
-                    Epoch(0),
-                    GAS_LIMIT_MULTIPLIER.into(),
-                    None,
-                ))));
-            tx.header.chain_id = shell.chain_id.clone();
-            tx.set_code(Code::new("wasm_code".as_bytes().to_owned(), None));
-            tx.set_data(Data::new(
-                format!("transaction data: {}", i).as_bytes().to_owned(),
-            ));
-            tx.add_section(Section::Signature(Signature::new(
-                tx.sechashes(),
-                [(0, keypair.clone())].into_iter().collect(),
-                None,
-            )));
-
-            let gas = Gas::from(
-                tx.header().wrapper().expect("Wrong tx type").gas_limit,
-            )
-            .checked_sub(Gas::from(tx.to_bytes().len() as u64))
-            .unwrap();
-            shell.enqueue_tx(tx.clone(), gas);
-            expected_wrapper.push(tx.clone());
-            req.txs.push(tx.to_bytes().into());
-            tx.update_header(TxType::Decrypted(DecryptedTx::Decrypted));
-            expected_decrypted.push(tx.clone());
-        }
-        // we extract the inner data from the txs for testing
-        // equality since otherwise changes in timestamps would
-        // fail the test
-        let expected_txs: Vec<Header> = expected_wrapper
-            .into_iter()
-            .chain(expected_decrypted.into_iter())
-            .map(|tx| tx.header)
-            .collect();
-        let received: Vec<Header> = shell
-            .prepare_proposal(req)
-            .txs
-            .into_iter()
-            .map(|tx_bytes| {
-                Tx::try_from(tx_bytes.as_ref()).expect("Test failed").header
-            })
-            .collect();
-        // check that the order of the txs is correct
-        assert_eq!(
-            received
-                .iter()
-                .map(|x| x.serialize_to_vec())
-                .collect::<Vec<_>>(),
-            expected_txs
-                .iter()
-                .map(|x| x.serialize_to_vec())
-                .collect::<Vec<_>>(),
-        );
     }
 
     /// Test that if the unsigned wrapper tx hash is known (replay attack), the

TuanTQ15/namada-1

crates/apps/src/lib/node/ledger/shell/prepare_proposal.rs
@@ -6,7 +6,6 @@ use namada::core::hints;
 use namada::core::key::tm_raw_hash_to_string;
 use namada::gas::TxGasMeter;
 use namada::ledger::protocol;
-use namada::ledger::storage::tx_queue::TxInQueue;
 use namada::proof_of_stake::storage::find_validator_by_raw_hash;
 use namada::state::{DBIter, StorageHasher, TempWlState, DB};
 use namada::tx::data::{DecryptedTx, TxType, WrapperTx};
@@ -16,8 +15,8 @@ use namada::vm::WasmCacheAccess;
 
 use super::super::*;
 use super::block_alloc::states::{
-    BuildingDecryptedTxBatch, BuildingProtocolTxBatch,
-    EncryptedTxBatchAllocator, NextState, TryAlloc,
+    BuildingProtocolTxBatch, BuildingTxBatch, NextState, TryAlloc,
+    WithNormalTxs, WithoutNormalTxs,
 };
 use super::block_alloc::{AllocFailure, BlockAllocator, BlockResources};
 use crate::config::ValidatorLocalConfig;
@@ -38,44 +37,46 @@ where
     ///
     /// INVARIANT: Any changes applied in this method must be reverted if
     /// the proposal is rejected (unless we can simply overwrite
-    /// them in the next block).
+    /// them in the next block). Furthermore, protocol transactions cannot
+    /// affect the ability of a tx to pay its wrapper fees.
     pub fn prepare_proposal(
         &self,
-        req: RequestPrepareProposal,
+        mut req: RequestPrepareProposal,
     ) -> response::PrepareProposal {
         let txs = if let ShellMode::Validator {
             ref local_config, ..
         } = self.mode
         {
             // start counting allotted space for txs
-            let alloc = self.get_encrypted_txs_allocator();
+            let alloc = self.get_protocol_txs_allocator();
+            // add vote extension protocol txs
+            let (alloc, mut txs) = self.build_protocol_txs(alloc, &mut req.txs);
+            let alloc = alloc.next_state();
 
             // add encrypted txs
             let tm_raw_hash_string =
                 tm_raw_hash_to_string(req.proposer_address);
-            let block_proposer =
-                find_validator_by_raw_hash(&self.state, tm_raw_hash_string)
-                    .unwrap()
-                    .expect(
-                        "Unable to find native validator address of block \
-                         proposer from tendermint raw hash",
-                    );
-            let (encrypted_txs, alloc) = self.build_encrypted_txs(
+            let block_proposer = find_validator_by_raw_hash(
+                &self.state,
+                tm_raw_hash_string,
+            )
+            .unwrap()
+            .expect(
+                "Unable to find native validator address of block proposer \
+                 from tendermint raw hash",
+            );
+            let (mut normal_txs, alloc) = self.build_normal_txs(
                 alloc,
                 &req.txs,
                 req.time,
                 &block_proposer,
                 local_config.as_ref(),
             );
-            let mut txs = encrypted_txs;
+            txs.append(&mut normal_txs);
             // decrypt the wrapper txs included in the previous block
-            let (mut decrypted_txs, alloc) = self.build_decrypted_txs(alloc);
-            txs.append(&mut decrypted_txs);
-
-            // add vote extension protocol txs
-            let mut protocol_txs = self.build_protocol_txs(alloc, &req.txs);
-            txs.append(&mut protocol_txs);
-
+            let (_, mut remaining_txs) =
+                self.build_protocol_txs(alloc, &mut req.txs);
+            txs.append(&mut remaining_txs);
             txs
         } else {
             vec![]
@@ -90,47 +91,28 @@ where
         response::PrepareProposal { txs }
     }
 
-    /// Depending on the current block height offset within the epoch,
-    /// transition state accordingly, return a block space allocator
-    /// with or without encrypted txs.
-    ///
-    /// # How to determine which path to take in the states DAG
-    ///
-    /// If we are at the second or third block height offset within an
-    /// epoch, we do not allow encrypted transactions to be included in
-    /// a block, therefore we return an allocator wrapped in an
-    /// [`EncryptedTxBatchAllocator::WithoutEncryptedTxs`] value.
-    /// Otherwise, we return an allocator wrapped in an
-    /// [`EncryptedTxBatchAllocator::WithEncryptedTxs`] value.
+    /// Get the first state of the block allocator. This is for protocol
+    /// transactions.
     #[inline]
-    fn get_encrypted_txs_allocator(&self) -> EncryptedTxBatchAllocator {
-        let is_2nd_height_off = self.is_deciding_offset_within_epoch(1);
-        let is_3rd_height_off = self.is_deciding_offset_within_epoch(2);
-
-        if hints::unlikely(is_2nd_height_off || is_3rd_height_off) {
-            tracing::warn!(
-                proposal_height =
-                    ?self.state.in_mem().block.height,
-                "No mempool txs are being included in the current proposal"
-            );
-            EncryptedTxBatchAllocator::WithoutEncryptedTxs(
-                (&*self.state).into(),
-            )
-        } else {
-            EncryptedTxBatchAllocator::WithEncryptedTxs((&*self.state).into())
-        }
+    fn get_protocol_txs_allocator(
+        &self,
+    ) -> BlockAllocator<BuildingProtocolTxBatch<WithNormalTxs>> {
+        (&self.state).into()
     }
 
     /// Builds a batch of encrypted transactions, retrieved from
     /// Tendermint's mempool.
-    fn build_encrypted_txs(
+    fn build_normal_txs(
         &self,
-        mut alloc: EncryptedTxBatchAllocator,
+        mut alloc: BlockAllocator<BuildingTxBatch>,
         txs: &[TxBytes],
         block_time: Option<Timestamp>,
         block_proposer: &Address,
         proposer_local_config: Option<&ValidatorLocalConfig>,
-    ) -> (Vec<TxBytes>, BlockAllocator<BuildingDecryptedTxBatch>) {
+    ) -> (
+        Vec<TxBytes>,
+        BlockAllocator<BuildingProtocolTxBatch<WithoutNormalTxs>>,
+    ) {
         let block_time = block_time.and_then(|block_time| {
             // If error in conversion, default to last block datetime, it's
             // valid because of mempool check
@@ -191,85 +173,24 @@ where
         (txs, alloc)
     }
 
-    /// Builds a batch of DKG decrypted transactions.
-    // NOTE: we won't have frontrunning protection until V2 of the
-    // Anoma protocol; Namada runs V1, therefore this method is
-    // essentially a NOOP
-    //
-    // sources:
-    // - https://specs.namada.net/main/releases/v2.html
-    // - https://github.com/anoma/ferveo
-    fn build_decrypted_txs(
-        &self,
-        mut alloc: BlockAllocator<BuildingDecryptedTxBatch>,
-    ) -> (Vec<TxBytes>, BlockAllocator<BuildingProtocolTxBatch>) {
-        let txs = self
-            .state
-            .in_mem()
-            .tx_queue
-            .iter()
-            .map(
-                |TxInQueue {
-                     tx,
-                     gas: _,
-                }| {
-                    let mut tx = tx.clone();
-                    tx.update_header(TxType::Decrypted(DecryptedTx::Decrypted));
-                    tx.to_bytes().into()
-                },
-            )
-            // TODO: make sure all decrypted txs are accepted
-            .take_while(|tx_bytes: &TxBytes| {
-                alloc.try_alloc(&tx_bytes[..]).map_or_else(
-                    |status| match status {
-                        AllocFailure::Rejected { bin_resource_left: bin_space_left } => {
-                            tracing::warn!(
-                                ?tx_bytes,
-                                bin_space_left,
-                                proposal_height =
-                                    ?self.get_current_decision_height(),
-                                "Dropping decrypted tx from the current proposal",
-                            );
-                            false
-                        }
-                        AllocFailure::OverflowsBin { bin_resource: bin_size } => {
-                            tracing::warn!(
-                                ?tx_bytes,
-                                bin_size,
-                                proposal_height =
-                                    ?self.get_current_decision_height(),
-                                "Dropping large decrypted tx from the current proposal",
-                            );
-                            true
-                        }
-                    },
-                    |()| true,
-                )
-            })
-            .collect();
-        let alloc = alloc.next_state();
-
-        (txs, alloc)
-    }
-
     /// Builds a batch of protocol transactions.
-    fn build_protocol_txs(
+    fn build_protocol_txs<M>(
         &self,
-        mut alloc: BlockAllocator<BuildingProtocolTxBatch>,
-        txs: &[TxBytes],
-    ) -> Vec<TxBytes> {
+        mut alloc: BlockAllocator<BuildingProtocolTxBatch<M>>,
+        txs: &mut Vec<TxBytes>,
+    ) -> (BlockAllocator<BuildingProtocolTxBatch<M>>, Vec<TxBytes>) {
         if self.state.in_mem().last_block.is_none() {
             // genesis should not contain vote extensions.
             //
             // this is because we have not decided any block through
             // consensus yet (hence height 0), which in turn means we
             // have not committed any vote extensions to a block either.
-            return vec![];
+            return (alloc, vec![]);
         }
 
-        let deserialized_iter = self.deserialize_vote_extensions(txs);
+        let mut deserialized_iter = self.deserialize_vote_extensions(txs);
 
-        deserialized_iter.take_while(|tx_bytes|
+        let taken = deserialized_iter.by_ref().take_while(|tx_bytes|
             alloc.try_alloc(&tx_bytes[..])
                 .map_or_else(
                     |status| match status {
@@ -307,7 +228,9 @@ where
                     |()| true,
                 )
         )
-        .collect()
+        .collect();
+        deserialized_iter.keep_rest();
+        (alloc, taken)
     }
 }
 
@@ -476,7 +399,7 @@ mod test_prepare_proposal {
     #[test]
     fn test_prepare_proposal_rejects_non_wrapper_tx() {
         let (shell, _recv, _, _) = test_utils::setup();
-        let mut tx = Tx::from_type(TxType::Decrypted(DecryptedTx::Decrypted));
+        let mut tx = Tx::from_type(TxType::Raw);
         tx.header.chain_id = shell.chain_id.clone();
         let req = RequestPrepareProposal {
             txs: vec![tx.to_bytes().into()],
@@ -762,100 +685,6 @@ mod test_prepare_proposal {
         };
 
         assert_eq!(signed_eth_ev_vote_extension, rsp_ext.0);
-    }
-
-    /// Test that the decrypted txs are included
-    /// in the proposal in the same order as their
-    /// corresponding wrappers
-    #[test]
-    fn test_decrypted_txs_in_correct_order() {
-        let (mut shell, _recv, _, _) = test_utils::setup();
-        let keypair = gen_keypair();
-        let mut expected_wrapper = vec![];
-        let mut expected_decrypted = vec![];
-
-        // Load some tokens to tx signer to pay fees
-        let balance_key = token::storage_key::balance_key(
-            &shell.state.in_mem().native_token,
-            &Address::from(&keypair.ref_to()),
-        );
-        shell
-            .state
-            .db_write(
-                &balance_key,
-                Amount::native_whole(1_000).serialize_to_vec(),
-            )
-            .unwrap();
-
-        let mut req = RequestPrepareProposal {
-            txs: vec![],
-            ..Default::default()
-        };
-        // create a request with two new wrappers from mempool and
-        // two wrappers from the previous block to be decrypted
-        for i in 0..2 {
-            let mut tx =
-                Tx::from_type(TxType::Wrapper(Box::new(WrapperTx::new(
-                    Fee {
-                        amount_per_gas_unit: DenominatedAmount::native(
-                            1.into(),
-                        ),
-                        token: shell.state.in_mem().native_token.clone(),
-                    },
-                    keypair.ref_to(),
-                    Epoch(0),
-                    GAS_LIMIT_MULTIPLIER.into(),
-                    None,
-                ))));
-            tx.header.chain_id = shell.chain_id.clone();
-            tx.set_code(Code::new("wasm_code".as_bytes().to_owned(), None));
-            tx.set_data(Data::new(
-                format!("transaction data: {}", i).as_bytes().to_owned(),
-            ));
-            tx.add_section(Section::Signature(Signature::new(
-                tx.sechashes(),
-                [(0, keypair.clone())].into_iter().collect(),
-                None,
-            )));
-
-            let gas = Gas::from(
-                tx.header().wrapper().expect("Wrong tx type").gas_limit,
-            )
-            .checked_sub(Gas::from(tx.to_bytes().len() as u64))
-            .unwrap();
-            shell.enqueue_tx(tx.clone(), gas);
-            expected_wrapper.push(tx.clone());
-            req.txs.push(tx.to_bytes().into());
-            tx.update_header(TxType::Decrypted(DecryptedTx::Decrypted));
-            expected_decrypted.push(tx.clone());
-        }
-        // we extract the inner data from the txs for testing
-        // equality since otherwise changes in timestamps would
-        // fail the test
-        let expected_txs: Vec<Header> = expected_wrapper
-            .into_iter()
-            .chain(expected_decrypted)
-            .map(|tx| tx.header)
-            .collect();
-        let received: Vec<Header> = shell
-            .prepare_proposal(req)
-            .txs
-            .into_iter()
-            .map(|tx_bytes| {
-                Tx::try_from(tx_bytes.as_ref()).expect("Test failed").header
-            })
-            .collect();
-        // check that the order of the txs is correct
-        assert_eq!(
-            received
-                .iter()
-                .map(|x| x.serialize_to_vec())
-                .collect::<Vec<_>>(),
-            expected_txs
-                .iter()
-                .map(|x| x.serialize_to_vec())
-                .collect::<Vec<_>>(),
-        );
     }
 
     /// Test that if the unsigned wrapper tx hash is known (replay attack), the